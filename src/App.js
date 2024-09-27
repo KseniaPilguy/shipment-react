08051ae4eae09ef3d5acf8368943cad47792c12e
Ksenia <ksenia.pilguy@scrumlaunch.com>
2024-09-27 18:50:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import { useParams } from "react-router-dom";
+
+const TABS = [
+  {
+    id: "dashboar",
+    title: "DASHBOARD",
+  },
+  {
+    id: "quote",
+    title: "QUICK QUOTE",
+  },
+  {
+    id: "rate",
+    title: "RATE & SHIP",
+  },
+  {
+    id: "history",
+    title: "HISTORY & TRACKING",
+  },
+  {
+    id: "account",
+    title: "MY ACCOUNT",
+  },
+];
+
+const App = () => {
+  const [activeTabId, setActiveTabId] = useState(null);
+  const { userId } = useParams();
+
+  const handleActiveTab = (id) => () => setActiveTabId(id);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="tabs_container">
+        {TABS.map(({ id, title }) => (
+          <div
+            className={`tab_container ${
+              activeTabId === id ? "tab_container_active" : ""
+            }`}
+            key={id}
+            onClick={handleActiveTab(id)}
+          >
+            {title}
+          </div>
+        ))}
+      </div>
+
+      <div className="content_container">
+        <div>{activeTabId}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
